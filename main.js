138217c6537fac4f26a7e873c5a140c29e6c7540
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-01-22 18:27:24 -0600

    got root.html linked and searchbar, btn on screen

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 console.log('Whim')
 
-angular.module('Whim', ['ngroute'])
+angular.module('Whim', ['ngRoute'])
     .config(($routeProvider) => {
         $routeProvider
         .when('/', {
             controller: 'RootCtrl',
             templateUrl: '/partials/root.html'
         })
+        .when('/HomePage/:home', {
+            controller: 'HomeCtrl',
+            templateUrl: '/partials/Category.html'
+        })
     })
+
+.controller('RootCtrl', function($scope, $location) {
+    console.log('I am a RootCtrl')
+
+    $scope.gotoCategory = () => {
+        // change the Url
+        $scope.gotoCategory = () => $location.url(`/HomePage/${$scope.home}`)
+    }
+})
